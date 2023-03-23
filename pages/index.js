150573899142338ff92dcfcd387159c35922ad45
Unam <97320894+manukao@users.noreply.github.com>
2023-03-23 15:58:54 +0100

    create random character backend api and display in frontend

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error } = useSWR("/api/random-character", fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>Random Character:</h1>
+      <p>First Name: {data.firstName}</p>
+      <p>Last Name: {data.lastName}</p>
+      <p>Twitter Name: {data.twitterName}</p>
+      <p>Geohash: {data.geohash}</p>
+    </div>
+  );
 }
